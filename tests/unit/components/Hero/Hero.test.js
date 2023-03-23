4f89ee8904a741fb529c6bd873d6f6c3fac88e61
Shatlyk Abdullayev <shatlykabdullayev@main.ru>
2023-03-23 18:02:19 +0300

    add hero test

--- tests/unit/components/Hero/Hero.test.js
+++ tests/unit/components/Hero/Hero.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/vue";
+import { nextTick } from "vue";
 
 import Hero from "@/components/Hero/Hero.vue";
 
@@ -10,5 +11,57 @@ describe("Hero", () => {
       name: /hero image/i,
     });
     expect(image).toBeInTheDocument();
+  });
+
+  it("renders action sentence ", () => {
+    render(Hero);
+
+    const actionSentence = screen.getByRole("heading", {
+      name: /Все о веб разработке/i,
+    });
+
+    expect(actionSentence).toBeInTheDocument();
+  });
+
+  it("checks whether setInterval was runned", () => {
+    vi.useFakeTimers();
+    const mock = vi.fn();
+    vi.stubGlobal("setInterval", mock);
+
+    render(Hero);
+    expect(mock).toHaveBeenCalled();
+    vi.useRealTimers();
+    vi.unstubAllGlobals();
+  });
+
+  it("swaps action word after interval", async () => {
+    vi.useFakeTimers();
+    render(Hero);
+
+    vi.advanceTimersToNextTimer();
+
+    await nextTick();
+
+    const actionSentence = screen.getByRole("heading", {
+      name: /Все о дизайн разработке/i,
+    });
+
+    expect(actionSentence).toBeInTheDocument();
+
+    vi.useRealTimers();
+  });
+
+  it("removes interval when component is unmounted", () => {
+    vi.useFakeTimers();
+    const mock = vi.fn();
+    vi.stubGlobal("clearInterval", mock);
+
+    const { unmount } = render(Hero);
+    unmount();
+
+    expect(mock).toHaveBeenCalled();
+
+    vi.useRealTimers();
+    vi.unstubAllGlobals();
   });
 });
--- tests/unit/components/Nav.test.js
+++ tests/unit/components/Nav.test.js
@@ -2,13 +2,17 @@ import Nav from "@/components/Nav.vue";
 import { render, screen } from "@testing-library/vue";
 
 describe("Nav", () => {
-  describe("nav list items", () => {
-    test("display nav items", () => {
-      render(Nav);
-      const allLists = screen.getAllByRole("listitem");
-
-      const allListNames = allLists.map((i) => i.textContent);
-      expect(allListNames).toEqual(["Поиск", "Все посты", "Обо мне"]);
+  test("display nav items", () => {
+    render(Nav, {
+      global: {
+        stubs: {
+          routerLink: true,
+        },
+      },
     });
+    const allLists = screen.getAllByRole("listitem");
+
+    const allListNames = allLists.map((i) => i.textContent);
+    expect(allListNames).toEqual(["Поиск", "Все посты", "Обо мне"]);
   });
 });
