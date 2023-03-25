6822a178e8c9e6643058a6e934b87ae7b952e2ec
Shatlyk Abdullayev <shatlykabdullayev@main.ru>
2023-03-25 10:47:38 +0300

    add font-awesome icon pack

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import router from "./router";
 import "./style.scss";
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+library.add(faSearch);
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount("#app");
