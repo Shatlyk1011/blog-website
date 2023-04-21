4dd5ad8c56846b001b4292dc1f9ce8b6bd41c587
Shatlyk Abdullayev <shatlykabdullayev@main.ru>
2023-04-21 15:25:37 +0300

    small bug fix

--- src/main.ts
+++ src/main.ts
@@ -26,8 +26,6 @@ import {
 import router from "./router";
 import "./style.scss";
 import App from "./App.vue";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase/config";
 
 library.add(
   faBold,
@@ -51,13 +49,7 @@ library.add(
   faQuoteLeft
 );
 
-let app: any;
-
-onAuthStateChanged(auth, () => {
-  if (!app) {
-    createApp(App)
-      .component("font-awesome-icon", FontAwesomeIcon)
-      .use(router)
-      .mount("#app");
-  }
-});
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount("#app");
