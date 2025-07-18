608c8cf8213fe8c2812805049cf637e2170e2b8e
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-07-18 20:44:34 +0500

    comment unused sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
-// Source_Sans_3
 import { Source_Sans_3 } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
