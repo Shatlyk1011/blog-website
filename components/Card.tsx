b38bb5e93fad912888cab42ac9a1044d062eb906
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-08-01 15:42:51 +0500

    fix dark bg in light mode

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ import Link from './Link'
 const Card = ({ title, description, imgSrc, href, figmaUrl, highlight }: Project) => (
   <div className="md max-w-[544px] p-4 md:w-1/2">
     <div
-      className={`${imgSrc && 'h-full'} ${highlight && 'bg-black/60'} overflow-hidden rounded-md border-2 border-gray-200/60 dark:border-gray-700/60`}
+      className={`${imgSrc && 'h-full'} ${highlight && 'bg-primary-50/40 dark:bg-black/65'} overflow-hidden rounded-md border-2 border-gray-200/60 dark:border-gray-700/60`}
     >
       {imgSrc &&
         (href ? (
